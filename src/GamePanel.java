02772a553f414536f4061c1710ea7de673425929
rylr <league@WTS9.attlocal.net>
2016-11-20 17:28:31 -0800

    gg
    gg

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,17 +15,17 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	final int GAME_STATE = 1;
 	final int END_STATE = 2;
 	int currentState = MENU_STATE;
-
+	Font title;
 	GamePanel() {
 		timer = new Timer(1000 / 60, this);
-
+		title = new Font("Arial", Font.PLAIN, 48);
 	}
 
 	void updateMenuState() {
 
 	}
 
-	void updateGameState() {
+   void updateGameState() {
 
 	}
 
@@ -32,15 +34,26 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	}
 
 	void drawMenuState(Graphics g) {
-
+		g.setColor(Color.BLUE);
+		g.fillRect(0, 0, Viking_War.height, Viking_War.width);
+		g.setFont(title);
+		g.setColor(Color.BLACK);
+		g.drawString("Viking War!", 650, 350);
+		g.drawString("Press Enter To Start", 550, 500);
 	}
 
 	void drawGameState(Graphics g) {
-
+		
+		g.clearRect(0, 0, Viking_War.height, Viking_War.width);
 	}
 
 	void drawEndState(Graphics g) {
-
+		g.setColor(Color.RED);
+		g.fillRect(0, 0, Viking_War.height, Viking_War.width);
+		g.setFont(title);
+		g.setColor(Color.BLACK);
+		g.drawString("You Lost!", 650, 350);
+		g.drawString("Press Enter To Go To The Start", 450, 500);
 	}
 
 	@Override
@@ -61,22 +74,41 @@ public class GamePanel extends JPanel implements ActionListener, KeyListener {
 	}
 
 	public void paintComponent(Graphics g) {
+		if (currentState == MENU_STATE) {
+			drawMenuState(g);
+		} else if (currentState == GAME_STATE) {
+			drawGameState(g);
+		} else if (currentState == END_STATE) {
+			drawEndState(g);
+		}
 
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		System.out.println("hi");
+		
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		System.out.println("hi");
+		if(e.getKeyCode() == KeyEvent.VK_ENTER){
+			if(currentState == MENU_STATE){
+				currentState = GAME_STATE;
+			}
+			else if(currentState == GAME_STATE)
+			{
+				currentState = END_STATE;
+			}
+			else if(currentState == END_STATE)
+			{
+				currentState = MENU_STATE;
+			}
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		System.out.println("hi");
+		
 	}
 
 }
